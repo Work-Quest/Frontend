3bffb7ff25fe78f04bbcfe104e26de8da7e76016
Napasorn <tonnam.napasorn@gmail.com>
2026-01-04 00:02:36 +0700

    add guard project page

---
+++ src/layouts/ProjectGuard.tsx
@@ -0,0 +1,34 @@
+import { Navigate, Outlet, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { get } from "@/Api"
+import NotFound from "@/pages/NotFound"
+
+export default function ProjectGuard() {
+  const { projectId } = useParams()
+  const [allowed, setAllowed] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    if (!projectId) return
+
+    const checkAccess = async () => {
+      try {
+        await get(`/api/project/access/${projectId}`)
+        setAllowed(true)
+      } catch {
+        setAllowed(false)
+      }
+    }
+
+    checkAccess()
+  }, [projectId])
+
+  if (allowed === null) {
+    return <div>Loading...</div>
+}
+
+  if (!allowed) {
+    return <div><NotFound></NotFound></div>
+  }
+
+  return <Outlet />
+}
