ac211f17ce263f7a88facbd5cf69e4319a21dfbf
AtikarnKr <nanokwok3131@gmail.com>
2026-01-20 00:49:13 +0700

    implement setup page with avatar and color selection

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,34 +1,30 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '@/components/Footer';
-import { useRef } from "react"
-import LoadingBar from "react-top-loading-bar"
-import { Toaster } from 'react-hot-toast';
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '@/components/Footer'
+import { useRef } from 'react'
+import LoadingBar from 'react-top-loading-bar'
+import { Toaster } from 'react-hot-toast'
 
 type MainLayoutProps = {
-  className?: string;
-};
+  className?: string
+  haveHeader?: boolean
+  haveFooter?: boolean
+}
 
-const MainLayout = ({ className = '' }: MainLayoutProps) => {
-   const loadingBarRef = useRef<any>(null)
-   // Expose globally so fetch/axios can control it
+const MainLayout = ({ className = '', haveHeader = true, haveFooter = true }: MainLayoutProps) => {
+  const loadingBarRef = useRef<any>(null)
   ;(window as any).loadingBarRef = loadingBarRef
   return (
     <div className={`flex flex-col min-h-screen ${className}`}>
-      <Toaster
-        position="bottom-left"
-        reverseOrder={false}
-        />
+      <Toaster position="bottom-left" reverseOrder={false} />
       <LoadingBar color="#f97316" ref={loadingBarRef} height={3} />
-      <div className="sticky top-0 z-50">
-        <Header />
-      </div>
-        <main className="flex-grow overflow-hidden">
+      <div className="sticky top-0 z-50">{haveHeader && <Header />}</div>
+      <main className="flex-grow overflow-hidden">
         <Outlet />
       </main>
-      <Footer />
+      {haveFooter && <Footer />}
     </div>
-  );
-};
+  )
+}
 
-export default MainLayout;
+export default MainLayout
--- src/main.tsx
+++ src/main.tsx
@@ -1,87 +1,88 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Landing from './pages/Landing.tsx';
-import Project from './pages/Project.tsx';
-import ProjectEnd from './pages/ProjectEnd.tsx';
-import Home from './pages/Home.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-import Profile from './pages/Profile.tsx';
-import NotFound from './pages/NotFound.tsx';
-import { AuthProvider } from "./context/AuthContext"
-import { GoogleOAuthProvider } from "@react-oauth/google"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Landing from './pages/Landing.tsx'
+import Project from './pages/Project.tsx'
+import ProjectEnd from './pages/ProjectEnd.tsx'
+import Home from './pages/Home.tsx'
+import Login from './pages/Login.tsx'
+import Register from './pages/Register.tsx'
+import Profile from './pages/Profile.tsx'
+import NotFound from './pages/NotFound.tsx'
+import { AuthProvider } from './context/AuthContext'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import AuthLayout from './layouts/AuthLayout.tsx'
 import MainLayout from './layouts/MainLayout.tsx'
-import ProtectedLayout from './layouts/ProtectedLayout.tsx';
-import ProjectGuard from './layouts/ProjectGuard.tsx';
+import ProtectedLayout from './layouts/ProtectedLayout.tsx'
+import ProjectGuard from './layouts/ProjectGuard.tsx'
+import Setup from './pages/Setup.tsx'
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <AuthLayout />, 
+    path: '/',
+    element: <AuthLayout />,
     children: [
-      { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
-    ]
+      { path: 'login', element: <Login /> },
+      { path: 'register', element: <Register /> },
+    ],
   },
 
   //public routes
   {
-    path: "/",
+    path: '/',
     element: <MainLayout />,
     children: [
-      { index: true, element: <Landing /> }, // ← THIS IS NOW PUBLIC
-      { path: "*", element: <NotFound /> },
+      { index: true, element: <Landing /> },
+      { path: 'setup', element: <Setup /> },
+      { path: '*', element: <NotFound /> },
     ],
   },
 
-  
   //protected routes
   {
-    path: "/",
-    element: <MainLayout />, 
+    path: '/',
+    element: <MainLayout />,
     children: [
-      { 
-        element: <ProtectedLayout />,  
+      {
+        element: <ProtectedLayout />,
         children: [
           { index: true, element: <Landing /> },
-          { path: "project-end", element: <ProjectEnd /> },
+          { path: 'project-end', element: <ProjectEnd /> },
           {
-            path: "project/:projectId",
+            path: 'project/:projectId',
             element: <ProjectGuard />,
-            children: [
-              { index: true, element: <Project /> }
-            ]
+            children: [{ index: true, element: <Project /> }],
           },
-          { path: "profile", element: <Profile /> },
-          
-        ]
-      }
-    ]
-    
-  }, 
+          { path: 'profile', element: <Profile /> },
+        ],
+      },
+    ],
+  },
   {
-    path: "/home",
-    element: <MainLayout className='bg-lightOrange'/>, 
+    path: '/home',
+    element: <MainLayout className="bg-white" />,
     children: [
-      { 
-        element: <ProtectedLayout />,  
-        children: [
-        { index: true, element: <Home />},
-        ]
-      }
-    ]
-  }
-  
-]);
+      {
+        element: <ProtectedLayout />,
+        children: [{ index: true, element: <Home /> }],
+      },
+    ],
+  },
+  {
+    path: '/setup',
+    element: <MainLayout haveHeader={false} haveFooter={false} />,
+    children: [
+      {
+        // element: <ProtectedLayout />,
+        children: [{ index: true, element: <Setup /> }],
+      },
+    ],
+  },
+])
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <AuthProvider>
@@ -89,5 +90,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       </AuthProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
-);
-
+)
